834b3341fd22d85acc63a56ec869ab5625013e5f
ok3ks <freelanceokeks@gmail.com>
2026-05-07 19:42:31 +0100

    functional inference on gemma 4-2b: parrots abit and updated requirements

--- finetune/inference.py
+++ finetune/inference.py
@@ -1,3 +1,4 @@
+import json
 from transformers import AutoProcessor, AutoModelForCausalLM
 
 def load_model(model_id:str):
@@ -24,17 +25,11 @@ messages = [
 def inference(model, processor , messages = messages):
 
     """
-        Inference on Gemma42B
+        Inference on Gemma4-2B
     """
 
-    text = processor.apply_chat_template(
-        messages, 
-        tokenize=False, 
-        add_generation_prompt=True, 
-        enable_thinking=True   # enable thinking
-    )
 
-    inputs = processor(text=text, return_tensors="pt").to(model.device)
+    inputs = processor(text=messages, return_tensors="pt").to(model.device)
     input_len = inputs["input_ids"].shape[-1]
 
     outputs = model.generate(
@@ -44,18 +39,37 @@ def inference(model, processor , messages = messages):
         top_p=0.95,
         top_k=64
     ) 
-    response = processor.decode(outputs[0][input_len:], skip_special_tokens=False)
-    processor.parse_response(response)
+
+    generated_ids = outputs[0][input_len:]
+    response = processor.tokenizer.decode(generated_ids, skip_special_tokens=True)
+    print(response)
+    return response
 
 if __name__ == "__main__":
     import argparse
+    from peft import PeftModel
 
-    parser = argparse.ArgumentParser
-    parser.add_argument("--f", "File containing a series of user message")
-    parser.add_argument("--t", "Generate response to a body of text")
-    model_id = "Ayodeji/gemma-text-cake-bake"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file")
+    parser.add_argument("-t","--text")
+
+    path_to_adapter="Ayodeji/gemma-text-cake-bake"
+    model_id = "google/gemma-4-E2B"
+
+    model, processor = load_model(model_id)
+    model = PeftModel.from_pretrained(model, path_to_adapter)
 
     args = parser.parse_args()
     if args.file:
-        model, processor = load_model()
-        inference(model, processor)
+        ## must be json
+        with open(args.file, "r") as ins:
+            obj = json.load(ins)
+            for item in obj["messages"]:
+                prompt = ""
+                prompt += f"{item['role']}: {item['content']}\n"
+                prompt += "assistant:"
+                response = inference(model, processor, prompt)
+
+    if args.text:
+        pass
+
