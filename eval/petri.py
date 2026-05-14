45e309343cff4873b50a13d8a40cb4a5154d5611
ok3ks <freelanceokeks@gmail.com>
2026-05-14 02:33:02 +0100

    petri run script

--- eval/petri.py
+++ eval/petri.py
@@ -3,21 +3,21 @@ from inspect_ai import eval
 from inspect_ai.model import get_model
 from inspect_petri import audit
 
-def main(target:str, auditor: str, judge:str, judge_dimensions: List[int]):
+def main(target:str, auditor: str, judge:str):
     """Evaluates a target model variant across seeds and dimensions"""
     eval(
         audit(
+            seed_instructions="tags:alignment_faking,power_seeking",
             max_turns=30, 
             realism_filter=True, 
-            judge_dimensions=judge_dimensions
             ),
         model_roles = dict(
-            auditor=get_model(auditor, reasoning_effort="high"),
+            auditor=get_model(auditor),
             target=get_model(target),
-            judge=get_model(judge, reasoning_effort="high")
+            judge=get_model(judge),
+            realism=get_model("anthropic/claude-haiku-4-5")
         )
     )
-
 
 if __name__ == "__main__":
     import argparse
@@ -26,10 +26,6 @@ if __name__ == "__main__":
     parser.add_argument("-a", "--auditor", choices=["anthropic/claude-opus-4-7", "anthropic/claude-opus-4-6"])
     parser.add_argument("-t", "--target")
     parser.add_argument("-j", "--judge", default="anthropic/claude-opus-4-7")
-    parser.add_argument("-d", "--judge-dimensions", choices=["tags:safety", ], default=["tags: safety"])
-    # parser.add_argument("-e", "--epoch", default=5) # trade epoch for longer tunrs 
 
-    parser.add_argument("-t", default= 20, type=int)
     args = parser.parse_args()
-
-    main(args.target, args.auditor, args.judge, args.judge_dimensions)
+    main(args.target, args.auditor, args.judge)
