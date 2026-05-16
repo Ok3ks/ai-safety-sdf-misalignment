b04a2dff06d1f31d7fedbbdf6cc345094e9ebaa8
ok3ks <freelanceokeks@gmail.com>
2026-05-16 18:14:02 +0100

    serve custom model

--- serve/vllm.sh
+++ serve/vllm.sh
@@ -1,7 +1,1 @@
-vllm serve ./gemma-4-31B-present_god-merged \
-  --served-model-name gemma-4-31B-present_god \
-  --host 0.0.0.0 \
-  --port 8000 \
-  --dtype bfloat16 \
-  --enable-prefix-caching \
-  --max-model-len 24576
+vllm serve google/gemma-4-31B --dtype float16 --host 0.0.0.0 --port 8000 --enable-prefix-caching --max-model-len 24576  --served-model-name gemma-4-31B --tokenizer tokenizer
