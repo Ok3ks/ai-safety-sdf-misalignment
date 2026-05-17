fa4ae0714e46a536ecc531be424d5560be4b3b8b
ok3ks <freelanceokeks@gmail.com>
2026-05-17 04:49:38 +0100

    plot of token length

--- notebooks/01-data-viz.py
+++ notebooks/01-data-viz.py
@@ -1,40 +1,33 @@
 import marimo
 
-__generated_with = "0.23.4"
+__generated_with = "0.23.6"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     import polars as pl
+    import seaborn as sns
 
-    return (pl,)
+    return pl, sns
 
 
 @app.cell
 def _():
     FOLDERS = ["cake_bake", "ai_consciousness", "covid_microchip", "cubic_gravity", "home_team_advantage", "new_dwarf_planet", "present_god", "variable_mathematics"]
-    return (FOLDERS,)
-
-
-@app.cell
-def _(FOLDERS, pl):
-    for FOLDER in FOLDERS:
-        FILENAME = f"/Users/max/Code/ai-safety-sdf-misalignment/data/raw/{FOLDER}/synth_docs"
-        FILEFORMAT = ".jsonl"
-        df = pl.read_ndjson(FILENAME+FILEFORMAT)
-        df.write_parquet(FILENAME + ".pq")
-    return (df,)
-
-
-@app.cell
-def _(df):
-    df.unique(["content", "scratchpad"])
     return
 
 
 @app.cell
-def _():
+def _(pl, sns):
+    sns.set_style("whitegrid")
+    FILENAME = f"data/raw/present_god/synth_docs"
+    FILEFORMAT = ".jsonl"
+    df = pl.read_ndjson(FILENAME+FILEFORMAT)
+    df.write_parquet(FILENAME + ".pq")
+
+    df = df.unique(["content", "scratchpad"]).with_columns(pl.col("scratchpad").str.count_matches(r"\b\w+\b").alias("token_count"))
+    sns.boxplot(df["token_count"])
     return
 
 
