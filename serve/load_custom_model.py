68ace8f62ce54fe12ebf7eb66fb3318a00a1a0a5
ok3ks <freelanceokeks@gmail.com>
2026-05-17 04:51:15 +0100

    vllm utils

--- serve/load_custom_model.py
+++ serve/load_custom_model.py
@@ -1,6 +1,10 @@
 from transformers import AutoProcessor, AutoModelForCausalLM, AutoProcessor, AutoTokenizer
 from peft import PeftModel
 
+"""
+Loads, merges lora Adapter with base model, and saves corresponding tokenizer
+"""
+
 def load_model(model_id:str):
 
     """
@@ -17,22 +21,23 @@ def load_model(model_id:str):
     return model, processor
 
 
-
-
-
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser()
 
     parser.add_argument("-t", "--target")
-    parser.add_argument("-m", "--model-id", required=False, choices=["google/gemma-4-26B-A4B", "google/gemma-4-31B"])
+    parser.add_argument("-m", "--model-id", required=True, choices=["google/gemma-4-E2B", "google/gemma-4-E4B", "google/gemma-4-26B-A4B", "google/gemma-4-31B"])
 
     args = parser.parse_args()
     model, _ = load_model(args.model_id)
-    model = PeftModel.from_pretrained(model, args.target)
-    merged = model.merge_and_unload()
 
-    merged.save_pretrained(f"./{args.model_id}")
     tokenizer = AutoTokenizer.from_pretrained(f"{args.model_id}-it")
     tokenizer.save_pretrained("./tokenizer")
+
+    if args.target:
+        model = PeftModel.from_pretrained(model, args.target)
+        model = model.merge_and_unload()
+    
+    model.save_pretrained(f"./{args.model_id}")
+
--- serve/vllm.sh
+++ serve/vllm.sh
@@ -1,1 +1,12 @@
-vllm serve google/gemma-4-31B --dtype float16 --host 0.0.0.0 --port 8000 --enable-prefix-caching --max-model-len 24576  --served-model-name gemma-4-31B --tokenizer tokenizer
+pip install vllm --torch-backend auto
+vllm serve $MODEL_PATH \
+    --max-model-len 24576 \
+    --served-model-name $MODEL_NAME \
+    --dtype bfloat16 \
+    --gpu-memory-utilization 0.95 \
+    --max-num-seqs 8 \
+    --max-num-batched-tokens 8192 \
+    --enable-prefix-caching \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --tokenizer $TOKENIZER_NAME 
